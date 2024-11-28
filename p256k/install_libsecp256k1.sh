c7321df1e436e0366362d8017a8be488ae70c0da
mleku <me@mleku.dev>
2024-11-28 09:47:35 +0000

    add secp256k1 build/install script

---
+++ p256k/install_libsecp256k1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+sudo apt -y install build-essential autoconf libtool
+cd $SCRIPT_DIR
+rm -rf secp256k1
+git clone https://github.com/bitcoin-core/secp256k1.git
+cd secp256k1
+git checkout v0.6.0
+git submodule init
+git submodule update
+./autogen.sh
+./configure --enable-module-schnorrsig --prefix=/usr
+make -j1
+sudo make install
