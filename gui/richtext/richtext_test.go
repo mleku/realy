8af41b89bd1e527a935d133bfe751df5aa4952cd
mleku <me@mleku.dev>
2024-12-29 20:55:57 -0106

    added richtext

---
+++ gui/richtext/richtext_test.go
@@ -0,0 +1,46 @@
+package richtext
+
+import (
+	"image"
+	"testing"
+	"time"
+
+	"gioui.org/font/gofont"
+	"gioui.org/io/input"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+)
+
+// TestNilInteractiveText ensures that it is safe to lay out
+// richtext with a nil state when none of the spans are
+// interactive.
+func TestNilInteractiveText(t *testing.T) {
+	th := material.NewTheme()
+	th.Shaper = text.NewShaper(text.WithCollection(gofont.Collection()))
+	spans := []SpanStyle{
+		{
+			Size:    12,
+			Content: "Hello",
+		},
+		{
+			Size:    12,
+			Content: "world",
+		},
+	}
+	var ops op.Ops
+	gtx := Gx{
+		Constraints: layout.Exact(image.Pt(100, 100)),
+		Metric: unit.Metric{
+			PxPerDp: 1,
+			PxPerSp: 1,
+		},
+		Source: input.Source{},
+		Now:    time.Now(),
+		Ops:    &ops,
+	}
+
+	Text(nil, th.Shaper, spans...).Layout(gtx)
+}
