943bf5152a27894e41fce604884c0134014eb783
mleku <me@mleku.dev>
2025-01-15 10:48:30 -0106

    create tool for updating util.go aliases

--- p256k/util.go
+++ p256k/util.go
@@ -1,5 +1,4 @@
 package p256k
-
 import (
 	"bytes"
 
@@ -8,12 +7,20 @@ import (
 )
 
 type (
-	bo = bool
-	by = []byte
-	st = string
-	er = error
-	no = int
-	cx = context.T
+	bo  = bool
+	by  = []byte
+	st  = string
+	er  = error
+	no  = int
+	i8  = int8
+	i16 = int16
+	i32 = int32
+	i64 = int64
+	u8  = uint8
+	u16 = uint16
+	u32 = uint32
+	u64 = uint64
+	cx  = context.T
 )
 
 var (
