d022023a4c93c7bd0dc792956ff3a8c58143b0bb
mleku <me@mleku.dev>
2025-03-23 08:54:42 -0106

    add json version of compact encoding with raw base64 url signature

---
+++ event/wirecompact.go
@@ -0,0 +1,36 @@
+package event
+
+import (
+	"encoding/base64"
+)
+
+// MarshalWireCompact encodes an event as the canonical form wrapped in an array
+// with the signature encoded in raw Base64 URL
+func (ev *T) MarshalWireCompact(dst []byte) (b []byte) {
+	b = dst
+	b = append(b, '[')
+	b = ev.ToCanonical(b)
+	b = append(b, ',', '"')
+	l := len(b)
+	b = append(b, make([]byte, 86)...)
+	base64.RawURLEncoding.Encode(b[l:], ev.Sig)
+	b = append(b, '"', ']')
+	return
+}
+
+func (ev *T) UnmarshalWireCompact(b []byte) (rem []byte, err error) {
+	startEv := 1
+	endSig := len(b) - 2
+	startSig := endSig - 86
+	endEv := startSig - 2
+	evB := b[startEv:endEv]
+	id := Hash(evB)
+	if rem, err = ev.FromCanonical(evB); chk.E(err) {
+		return
+	}
+	sigB := make([]byte, 64)
+	_, err = base64.RawURLEncoding.Decode(sigB, b[startSig:endSig])
+	ev.Sig = sigB
+	ev.ID = id
+	return
+}
